98a177b6ec630030c34ff91dd0ccd52e6ffc2a88
jiaderc <jiaderc@ifi.uio.no>
2017-05-18 19:53:17 +0200

    Ferdig med tabeller osv

--- Code/data_seg.cpp
+++ Code/data_seg.cpp
@@ -1,18 +1,20 @@
 //sensor 0
-//raw data that continuously storing sensor data
+//Raw data that continuously storing sensor data
 std::vector<std::vector<float>> rawDataS0; 
-//
+//Used to detect whether the foot is on ground or not
 std::vector<std::vector<float>> stabilityLst;
-//contains sensor data when the foot is on ground
+//Contains sensor data when the foot is on ground
 std::vector<std::vector<float>> interestDataS0;
-int fyll = 0; 
+int fill = 0; 
 int start_to_fill = 0; //Enables when the foot is on ground
 
-void print_s(int sensorNr){
+//This function check whether the stored sensor data does not exceed maxBoundary.
+//If the 
+void checkDesiredData(int sensorNr)
+{
     int maxBoundary = 145;
     if(sensorNr == 0) {
-        interestDataS0.resize(interestDataS0.size() - stabilityLst.size());
-
+        interestDataS0.resize(interestDataS0.size() - stabilityLst.size());		
         if (interestDataS0.size() < expected_length_data ||
             interestDataS0.size() > maxBoundary) {
             interestDataS0.clear();
@@ -22,16 +24,17 @@ void print_s(int sensorNr){
     writeToFile(); //A function that write the data to file (not shown in code)
 }
 
-int isStable(std::vector<std::vector<float>> stabArr){
+int isStable(std::vector<std::vector<float>> stabilityArray)
+{
     float boundary = 0.009;
     int stabilityCount = 0;
-    int pos1 = stabArr.size()-2;
-    int pos2 = stabArr.size()-1;
+    int pos1 = stabilityArray.size()-2;
+    int pos2 = stabilityArray.size()-1;
 
     int i;
     for(i=0;i<3;i++){
-        float current = stabArr.at(pos1).at(i);
-        float next = stabArr.at(pos2).at(i);
+        float current = stabilityArray.at(pos1).at(i);
+        float next = stabilityArray.at(pos2).at(i);
 
         if (fabs(next - current) > boundary) {
             return 0;
@@ -40,12 +43,13 @@ int isStable(std::vector<std::vector<float>> stabArr){
     return 1;
 }
 
-void handleDataS0(){
+void handleDataS0()
+{
     if(stabilityLst.size() > 2){
-        if(fyll == 0 && isStable(stabilityLst)){
-            fyll = 1;
+        if(fill == 0 && isStable(stabilityLst)){
+            fill = 1;
         }
-        else if(fyll) {
+        else if(fill) {
             if (!isStable(stabilityLst)) {
 
                 if (stabilityLst.size() > stabThres) {
@@ -55,7 +59,7 @@ void handleDataS0(){
                     interestDataS0.push_back(stabilityLst.at(secondLast));
                     interestDataS0.push_back(stabilityLst.at(lastElement));
                 }
-                fyll = 0;
+                fill = 0;
                 stabilityLst.clear();
             }else if( start_to_fill && stabilityLst.size() >= stabThres  ){
                 start_to_fill = 0;
@@ -63,17 +67,16 @@ void handleDataS0(){
             }
         }
         else{
-            fyll = 0;
+            fill = 0;
             stabilityLst.clear();
         }
     }
 }
 
-//This function obtain sensor data
+//This function obtains sensor data
 void optoforceCallback0(const geometry_msgs::WrenchStamped::ConstPtr& msg)
 {
     std::vector<float> tmp;
-	
 	//Storing sensor data
     tmp.push_back(msg->wrench.force.x);
     tmp.push_back(msg->wrench.force.y);
